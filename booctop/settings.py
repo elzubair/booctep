061fe0cbe0fd6ceba42d521f07d13c85ce10b292
Elzubair Mohammed <alzober1414@gmail.com>
2021-12-22 17:31:27 +0800

    elzubair

--- booctop/settings.py
+++ booctop/settings.py
@@ -141,7 +141,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'booctop',
         'USER': 'root', # 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost', #'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
